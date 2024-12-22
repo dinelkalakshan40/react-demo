21891c25fd75d475b331265a944914bfe2d76f2f
dinelka <newlakshan3@gmail.com>
2024-12-22 23:21:49 +0530

    define and call an object

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,20 @@ function App() {
     }
     const total =getTotal(10,20)
 
+    const person = {
+        name : "Dinelka Lakshan",
+        age : 25,
+        address : "Panadura",
+    }
     return (
         <>
             <h1 className="heading">React</h1>
             <p>{a}</p>
             <p>{message}</p>
-            <h2>the Total is{total}</h2>
+            <h2>The Total is{total}</h2> <br/>
+            <h3>Name : {person.name}</h3>
+            <h3>Age : {person.age}</h3>
+            <h3>Address : {person.address}</h3>
         </>
     )
 }
