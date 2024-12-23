b3f37b70f444a71f15cd643b9ac73febb7a6f028
dinelka <newlakshan3@gmail.com>
2024-12-23 14:28:24 +0530

    create component

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
 import './App.css'
+import Component from "./Component/Component.tsx";
 
 
 function App() {
-    const a = 30;
 
-    const message = "Welcome to React with TypeScript"
-
-    const getTotal =(num1:number,num2:number):number=>{
-        return num1 + num2;
-    }
-    const total =getTotal(10,20)
-
-    const person = {
-        name : "Dinelka Lakshan",
-        age : 25,
-        address : "Panadura",
-    }
-    //array
-    const fruits :string[] =["Apple", "Banana", "Orange"];
-    const handleClick = () :void => {
-        alert("button clicked");
-    };
     return (
         <>
-            <h1 className="heading">React</h1>
-            <p>{a}</p>
-            <p>{message}</p>
-            <h2>The Total is{total}</h2> <br/>
-            <h3>Name : {person.name}</h3>
-            <h3>Age : {person.age}</h3>
-            <h3>Address : {person.address}</h3>
-            <h2>{fruits.map((fruit, index) => (
-                <li key={index}>{fruit}</li>
-            ))}</h2>
+            <Component/>
 
-            <button onClick={handleClick}> Click Me</button>
         </>
     )
 }
---
+++ src/Component/Component.tsx
@@ -0,0 +1,7 @@
+function Component(){
+    return(
+        <div>Hello</div>
+    )
+}
+
+export default Component
