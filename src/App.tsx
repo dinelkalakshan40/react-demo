87f6c0b2163c72c2b47ffb6042009b5397d733b1
dinelka <newlakshan3@gmail.com>
2024-12-23 19:48:13 +0530

    children  props

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
     return (
         <>
             <Component/> {/*use componnent*/}
-            <TextBox name="Dinelka" id="S001" address="Panadura"/>
+            <TextBox name="Dinelka" id="S001" address="Panadura">
+                <p>This is a Discription</p>
+            </TextBox>
             <TextBox name="Lakshan" id="S002" address="Galee"/>
             <TextBox name="Perera" id="S003" address="Colombo"/>
 
--- src/Component/TextBox.tsx
+++ src/Component/TextBox.tsx
@@ -4,11 +4,14 @@ type props = {
     name: string;
     id: string;
     address: string;
+    children?: React.ReactNode;
 };
-function TextBox({ name, id, address }:props){
+function TextBox({ name, id, address,children }:props){
     return(
         <div className="textBox">
             Student name: {name}, Id: {id}, Address: {address}
+
+            {children && <div className="description">{children}</div>}
         </div>
     )
 }
