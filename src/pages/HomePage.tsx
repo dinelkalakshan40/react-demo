ac2e399baf59797cdde9ad8cb627bd032c5f18c2
dinelka <newlakshan3@gmail.com>
2024-12-24 14:13:35 +0530

    router with link

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
+import {Link} from "react-router";
 
 export function HomePage(){
     return(
         <div>
             <h1>Home-Page</h1>
+            <Link to={'/about'}>go to about page</Link><br/>
+            <Link to={'/contact'}>go to contact  page</Link><br/>
+
         </div>
     )
 }
