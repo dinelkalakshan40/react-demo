01088711e387ae7aaee0b5452c66152cf7d6b833
dinelka <newlakshan3@gmail.com>
2024-12-24 18:08:18 +0530

    useRef manupilete Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
-import {BrowserRouter, Route, Routes} from "react-router";
-import {HomePage} from "./pages/HomePage.tsx";
-import {AboutPage} from "./pages/AboutPage.tsx";
-import {ContactPage} from "./pages/ContactPage.tsx";
+import {useRef} from "react";
 
 
 function App() {
 
+    // Define the ref with type HTMLInputElement or null
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleClick = () => {
+        // Check if the ref is not null
+        if (inputRef.current) {
+            const value = inputRef.current.value;
+
+            if (value === "") {
+                inputRef.current.focus();
+            } else if (Number(value) > 50) {
+                alert("Success");
+            } else {
+                alert("Error");
+            }
+        }
+    };
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage/>}></Route>
-                <Route path="/about" element={<AboutPage/>}></Route>
-                <Route path="/contact" element={<ContactPage/>}></Route>
-
-            </Routes>
-        </BrowserRouter>
-
+        <>
+            <input type="text" ref={inputRef} placeholder="Enter a number"/>
+            <button onClick={handleClick}>Validate</button>
+        </>
     );
 }
 
